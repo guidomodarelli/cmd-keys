aa7d89859ee0243e18bd5b7c9dcb0c75eea0ef4e
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-06 15:24:47 -0300

    refactor: update constants to use const objects and lowercase values

--- src/components/cmd-header/constants.ts
+++ src/components/cmd-header/constants.ts
@@ -1,12 +1,12 @@
-export enum Attributes {
-  PLACEHOLDER = "placeholder",
-  HIDE_BREADCRUMBS = "hideBreadcrumbs",
-  BREADCRUMB_HOME = "breadcrumbHome",
-  BREADCRUMBS = "breadcrumbs",
-}
+export const Attributes = {
+  PLACEHOLDER: "placeholder",
+  HIDE_BREADCRUMBS: "hide-breadcrumbs",
+  BREADCRUMB_HOME: "breadcrumb-home",
+  BREADCRUMBS: "breadcrumbs",
+} satisfies Record<string, Lowercase<string>>;
 
-export enum CmdHeaderEventType {
-  SET_PARENT = "setParent",
-  CHANGE = "change",
-  CLOSE = "close",
-}
+export const CmdHeaderEventType = {
+  SET_PARENT: "set-parent",
+  CHANGE: "change",
+  CLOSE: "close",
+} satisfies Record<string, Lowercase<string>>;
