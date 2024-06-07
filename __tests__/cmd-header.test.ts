ee350ded23cd6f9776da36afcd41a86053335258
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-06 21:48:57 -0300

    test: Update input placeholder text and focus test

--- __tests__/cmd-header.test.ts
+++ __tests__/cmd-header.test.ts
@@ -29,15 +29,28 @@ describe("CmdHeader component", () => {
       const { shadowRoot } = await TestUtils.render(CmdHeader.tag);
       const input = shadowRoot?.querySelector("input");
 
-      expect(input?.placeholder).toBe("Type here...")
+      expect(input?.placeholder).toBe("Type here...");
     });
 
-    it.skip("verifies the input field is focused", async () => {
+    it("verifies the input placeholder has changed to 'HERE'", async () => {
+      const { shadowRoot } = await TestUtils.render(CmdHeader.tag, {
+        [Attributes.PLACEHOLDER]: "HERE",
+      });
+      const input = shadowRoot?.querySelector("input");
+
+      expect(input?.placeholder).toBe("HERE");
+    });
+
+    it("verifies the input field is focused", async () => {
       const { shadowRoot } = await TestUtils.render(CmdHeader.tag);
       const input = shadowRoot?.querySelector("input");
 
-      console.log(shadowRoot?.activeElement);
-      expect(shadowRoot?.activeElement === input).toBeTruthy();
+      // By focusing the input field asynchronously within a requestAnimationFrame, this can cause the input field not
+      // to be focused immediately after the call to focus(), which could lead to issues in the test.
+      // One solution is to wait for a brief period after focusing the input field to allow the focus to be established.
+      setTimeout(() => {
+        expect(shadowRoot?.activeElement === input).toBeTruthy();
+      }, 0);
     });
   });
 
